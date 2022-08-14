44ed4bd5e03024e0537f95c265abfa2609e7f2bd
Zuza Zuber <zuza@lewagon.org>
2022-08-14 18:30:46 +0200

    setup app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/posts/:id", to: "posts#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
