cbb4474871d28a9b0b608278a38d03b3928ed53c
Zuza Zuber <zuza@lewagon.org>
2022-09-12 12:44:54 +0200

    set up meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper ActionText::Engine.helpers
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
